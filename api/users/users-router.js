49497144c83a421a2df0ed28fcbbd437c4250c79
Yigit Zarbun <yzarbun@gmail.com>
2023-02-23 14:19:09 +0300

    login özelliği eklendi, ancak kullanıcı bilgileri validasyonu middleware yerine router'da yapılıyor

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -37,4 +37,26 @@ router.post("/kayitol", md.validateUser, (req, res) => {
   res.status(200).json(newUser);
 });
 
+router.post("/giris", md.validateUser, (req, res) => {
+  //İLK OLARAK LOGIN EKRANINDA KULLANICI BİLGİLERİ MEVCUT MU KONTROL
+  //DAHA SONRA KULLANICI BİLGİLERİ DB'DE MEVCUT MU KONTROL
+  let loginUser = {
+    username: req.username,
+    password: req.password,
+  };
+
+  let isAuth = false;
+
+  for (let i = 0; i < users.length; i++) {
+    if (
+      users[i].username === loginUser.username &&
+      users[i].password === loginUser.password
+    ) {
+      isAuth = true;
+    }
+  }
+  isAuth
+    ? res.status(200).json({ message: `hoşgeldin ${loginUser.username}` })
+    : res.status(400).json({ message: "hatalı giriş bilgileri" });
+});
 module.exports = router;
